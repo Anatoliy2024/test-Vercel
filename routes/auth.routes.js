4b0b045f22335f232c36f0595484fa7fbee29921
Anatoliy2024 <gredlend20@gmail.com>
2024-06-16 02:07:21 +0300

    1

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -11,101 +11,101 @@ const router = new Router()
 
 const authMiddleware = require("../middleware/auth.middleware")
 
-router.post(
-  "/registration",
-  [
-    check(
-      "password",
-      "Password must be longer than 3 and shorter than 12"
-    ).isLength({ min: 3, max: 12 }),
-  ],
-  async (req, res) => {
-    try {
-      //   console.log(req.body)
-      const errors = validationResult(req)
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ message: "Uncorrect request", errors })
-      }
+// router.post(
+//   "/registration",
+//   [
+//     check(
+//       "password",
+//       "Password must be longer than 3 and shorter than 12"
+//     ).isLength({ min: 3, max: 12 }),
+//   ],
+//   async (req, res) => {
+//     try {
+//       //   console.log(req.body)
+//       const errors = validationResult(req)
+//       if (!errors.isEmpty()) {
+//         return res.status(400).json({ message: "Uncorrect request", errors })
+//       }
 
-      const { name, password } = req.body
-      const candidate = await User.findOne({ name })
-      if (candidate) {
-        return res
-          .status(400)
-          .json({ message: `User with ${name} already exist` })
-      }
-      const hashPassword = await bcrypt.hash(password, 8)
-      const user = new User({
-        name,
-        password: hashPassword,
-        avatar: "",
-      })
-      await user.save()
-      return res.json({ message: "User was create" })
-    } catch (e) {
-      console.log(e)
-      res.send({ message: "Server Error" })
-    }
-  }
-)
+//       const { name, password } = req.body
+//       const candidate = await User.findOne({ name })
+//       if (candidate) {
+//         return res
+//           .status(400)
+//           .json({ message: `User with ${name} already exist` })
+//       }
+//       const hashPassword = await bcrypt.hash(password, 8)
+//       const user = new User({
+//         name,
+//         password: hashPassword,
+//         avatar: "",
+//       })
+//       await user.save()
+//       return res.json({ message: "User was create" })
+//     } catch (e) {
+//       console.log(e)
+//       res.send({ message: "Server Error" })
+//     }
+//   }
+// )
 
-router.post("/login", async (req, res) => {
-  try {
-    const { name, password } = req.body
-    const user = await User.findOne({ name })
-    if (!user) {
-      return res.status(404).json({ message: "User not found" })
-    }
-    const isPassValid = bcrypt.compareSync(password, user.password)
-    if (!isPassValid) {
-      return res.status(400).json({ message: "Invalid password" })
-    }
-    const token = jwt.sign(
-      { id: user.id, name: user.name, avatar: user.avatar },
-      config.get("secretKey"),
-      {
-        expiresIn: "1h",
-      }
-    )
+// router.post("/login", async (req, res) => {
+//   try {
+//     const { name, password } = req.body
+//     const user = await User.findOne({ name })
+//     if (!user) {
+//       return res.status(404).json({ message: "User not found" })
+//     }
+//     const isPassValid = bcrypt.compareSync(password, user.password)
+//     if (!isPassValid) {
+//       return res.status(400).json({ message: "Invalid password" })
+//     }
+//     const token = jwt.sign(
+//       { id: user.id, name: user.name, avatar: user.avatar },
+//       config.get("secretKey"),
+//       {
+//         expiresIn: "1h",
+//       }
+//     )
 
-    return res.json({
-      token,
-      user: {
-        id: user.id,
-        name: user.name,
-        avatar: user.avatar,
-      },
-    })
-  } catch (e) {
-    console.log(e)
-    res.send({ message: "Server Error" })
-  }
-})
+//     return res.json({
+//       token,
+//       user: {
+//         id: user.id,
+//         name: user.name,
+//         avatar: user.avatar,
+//       },
+//     })
+//   } catch (e) {
+//     console.log(e)
+//     res.send({ message: "Server Error" })
+//   }
+// })
 
-router.get("/auth", authMiddleware, async (req, res) => {
-  try {
-    const user = await User.findOne({ _id: req.user.id })
+// router.get("/auth", authMiddleware, async (req, res) => {
+//   try {
+//     const user = await User.findOne({ _id: req.user.id })
 
-    const token = jwt.sign(
-      { id: user.id, name: user.name, avatar: user.avatar },
-      config.get("secretKey"),
-      {
-        expiresIn: "1h",
-      }
-    )
+//     const token = jwt.sign(
+//       { id: user.id, name: user.name, avatar: user.avatar },
+//       config.get("secretKey"),
+//       {
+//         expiresIn: "1h",
+//       }
+//     )
 
-    return res.json({
-      token,
-      user: {
-        id: user.id,
-        name: user.name,
-        avatar: user.avatar,
-      },
-    })
-  } catch (e) {
-    console.log(e)
-    res.send({ message: "Server Error" })
-  }
-})
+//     return res.json({
+//       token,
+//       user: {
+//         id: user.id,
+//         name: user.name,
+//         avatar: user.avatar,
+//       },
+//     })
+//   } catch (e) {
+//     console.log(e)
+//     res.send({ message: "Server Error" })
+//   }
+// })
 
 module.exports = router
