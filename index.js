9165a62f83743e45b49b2e4f8e38efedfe5584a7
Anatoliy2024 <gredlend20@gmail.com>
2024-06-15 18:12:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express()
+const port = 9000
+
+app.use("/", (req, res) => {
+  res.json({ message: "Hellow,World" })
+})
+
+app.listen(9000, () => {
+  console.log(`Starting Server on port ${port}`)
+})
