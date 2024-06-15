b3b6d6a831e98e88a0c31dd992d2209b5bbc31b5
Anatoliy2024 <gredlend20@gmail.com>
2024-06-15 19:38:16 +0300

    5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express"
 
 const app = express()
-const port = process.env.PORT
+// const port = process.env.PORT
 
 app.get("/", (req, res) => {
   res.json({ message: "Hellow,World" })
@@ -10,6 +10,6 @@ app.get("/test", (req, res) => {
   res.json({ message: "Test" })
 })
 
-app.listen(port, () => {
-  console.log(`Starting Server on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Starting Server on port ${port}`)
+// })
