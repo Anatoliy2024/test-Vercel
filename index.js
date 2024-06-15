ff311bc29a49f944b50b51e7f76354dcb755e0e5
Anatoliy2024 <gredlend20@gmail.com>
2024-06-15 18:45:02 +0300

    tets

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 9000
 app.use("/", (req, res) => {
   res.json({ message: "Hellow,World" })
 })
+app.use("/test", (req, res) => {
+  res.json({ message: "Test" })
+})
 
 app.listen(9000, () => {
   console.log(`Starting Server on port ${port}`)
